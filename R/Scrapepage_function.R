34eeb1d2a83a760fc249a9016daac0eed02c3436
Jakub Szabó <jakubszabo4@gmail.com>
2024-06-14 12:46:51 +0200

    Update Scrapepage_function.R

--- R/Scrapepage_function.R
+++ R/Scrapepage_function.R
@@ -1,13 +1,14 @@
-# Webscrape the content of the specific page 
-scrape_page <- function(page_number) {
-  navigate_to_page(page_number)
-  Sys.sleep(3)  # Wait for the page to load
+# Since the `Getlastpagenum_function.R` returns the page back to the first page, this function webscrappes the content of the first page 
+scrape_first_page <- function(){
   page_source <- remDr$getPageSource()[[1]]
   result <- webscrape(page_source)
   return(result)
 }
 
-scrape_first_page <- function(){
+# Function webscrapping the content of a specific page based on the page number 
+scrape_page <- function(page_number) {
+  navigate_to_page(page_number)
+  Sys.sleep(3)  # Wait for the page to load
   page_source <- remDr$getPageSource()[[1]]
   result <- webscrape(page_source)
   return(result)
