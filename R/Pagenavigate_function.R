d0a77f4df123553bfb3cb4c294af74bfb08329fc
Jakub Szabó <jakubszabo4@gmail.com>
2024-06-14 12:44:35 +0200

    Update Pagenavigate_function.R

--- R/Pagenavigate_function.R
+++ R/Pagenavigate_function.R
@@ -1,4 +1,7 @@
-# Navigate to a concrete page 
+# Function that navigates to any page based on the page number 
+# Only works in my specific loop that iterates through each page one by one 
+# As the pagination is dynamic, it shows buttons redirecting to the first and last page and only ten pages at once. Thus, for instance, choosing page_number 1250 whilst being at page 25 would end in error since the attribute "a href" with javascript::..., `Page$1250` is not definied in the td class at page number 25
+
 navigate_to_page <- function(page_number) {
   page_number_str <- paste0("Page$", page_number)
   xpath <- sprintf("//a[contains(@href, '%s')]", page_number_str)
